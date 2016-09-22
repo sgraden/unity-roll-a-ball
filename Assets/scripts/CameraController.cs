394cb8c408941c07c9034b7170d6d7cc6323394c
STEVEN G. RADEN <sgraden@us.ibm.com>
2016-09-22 14:06:10 -0500

    added pickup objects and game walls.

---
+++ Assets/scripts/CameraController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+
+	public GameObject player;
+
+	private Vector3 offset;
+
+	// Use this for initialization
+	void Start () {
+		offset = transform.position - player.transform.position;
+	}
+	
+	// LateUpdate will only update once all other updates have been set
+	void LateUpdate () {
+		transform.position = player.transform.position + offset;
+	}
+}
