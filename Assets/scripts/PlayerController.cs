7785ecae02b8f413c058ba47b4e4107b003eb28c
STEVEN G. RADEN <sgraden@us.ibm.com>
2016-10-18 18:01:51 -0500

    finished the game! Exported and built for all to enjoy

--- Assets/scripts/PlayerController.cs
+++ Assets/scripts/PlayerController.cs
@@ -1,14 +1,22 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour {
 
+	public Text countText;
+	public Text winText;
+
 	public float speed;
+	private int count;
 
 	private Rigidbody rb;
 
 	void Start () {
 		rb = GetComponent<Rigidbody>();
+		count = 0;
+		SetCountText();
+		winText.text = "";
 	}
 
 	void FixedUpdate () {
@@ -24,6 +32,15 @@ public class PlayerController : MonoBehaviour {
 	void OnTriggerEnter(Collider other) {
 		if (other.gameObject.CompareTag ("Pickup")) {
 			other.gameObject.SetActive (false);
+			count++;
+			SetCountText ();
+		}
+	}
+
+	void SetCountText () {
+		countText.text = "Count: " + count.ToString ();
+		if (count >= 12) {
+			winText.text = "You Win!!!";
 		}
 	}
 }
